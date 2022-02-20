6c145b5256e876648433074330de01ddfb8ca833
Amith-B <amithbr6@gmail.com>
2022-02-20 23:27:20 +0530

    removed fullscreen from editNote dialog

--- src/components/notes/EditNote.tsx
+++ src/components/notes/EditNote.tsx
@@ -9,19 +9,20 @@ import {
   Notes,
 } from "../../models/Note";
 import {
-  AppBar,
   Box,
   Button,
   Container,
   Dialog,
-  Toolbar,
   IconButton,
   Slide,
-  Typography,
   TextField,
   TextareaAutosize,
   Divider,
   LinearProgress,
+  DialogTitle,
+  DialogTitleProps,
+  DialogContent,
+  DialogActions,
 } from "@mui/material";
 import { fontColor } from "../../helpers";
 import { selectNotesStatus } from "../../store/reducers/notesSlice";
@@ -35,6 +36,32 @@ const Transition = React.forwardRef(function Transition(
 ) {
   return <Slide direction="up" ref={ref} {...props} />;
 });
+
+const BootstrapDialogTitle = (
+  props: DialogTitleProps & { onClose: () => void }
+) => {
+  const { children, onClose, ...other } = props;
+
+  return (
+    <DialogTitle sx={{ m: 0, p: 2 }} {...other}>
+      {children}
+      {onClose ? (
+        <IconButton
+          aria-label="close"
+          onClick={onClose}
+          sx={{
+            position: "absolute",
+            right: 8,
+            top: 8,
+            color: (theme) => theme.palette.grey[500],
+          }}
+        >
+          <CloseIcon />
+        </IconButton>
+      ) : null}
+    </DialogTitle>
+  );
+};
 
 export default function EditNote({
   handleClose,
@@ -66,10 +93,15 @@ export default function EditNote({
   return (
     <div>
       <Dialog
-        fullScreen
         open={open}
         onClose={handleClose}
         TransitionComponent={Transition}
+        fullWidth
+        sx={{
+          "& .MuiBackdrop-root": {
+            backdropFilter: "blur(10px)",
+          },
+        }}
       >
         <Box
           sx={{
@@ -79,85 +111,106 @@ export default function EditNote({
             height: "100%",
           }}
         >
-          <AppBar sx={{ position: "relative" }}>
-            <Toolbar>
-              <IconButton
-                edge="start"
-                color="inherit"
-                onClick={handleClose}
-                aria-label="close"
-              >
-                <CloseIcon />
-              </IconButton>
-              <Typography sx={{ ml: 2, flex: 1 }} variant="h6" component="div">
-                {note ? "Edit Note" : "Create Note"}
-              </Typography>
-              <Button
-                autoFocus
-                color="inherit"
-                onClick={() => handleSave(noteDetails, note ? "edit" : "add")}
-              >
-                Save
-              </Button>
-            </Toolbar>
-          </AppBar>
-          {notesStatus === "loading" && <LinearProgress />}
-          <Container
-            maxWidth="xl"
+          <BootstrapDialogTitle
+            id="customized-dialog-title"
+            onClose={handleClose}
             sx={{
-              flexBasis: "auto",
-              height: "100%",
-              overflow: "auto",
               background: COLORS[noteDetails.color],
-              "& .MuiInputBase-root, & .MuiInputLabel-root": {
+              color: fontColor(noteDetails.color),
+              "& .MuiButtonBase-root": {
                 color: fontColor(noteDetails.color),
-              },
-              "& .MuiOutlinedInput-root": {
-                "& fieldset, &:hover fieldset, &.Mui-focused fieldset": {
-                  borderColor: fontColor(noteDetails.color),
-                },
               },
             }}
           >
-            <TextField
-              fullWidth
-              label="Title"
-              id="title"
-              value={noteDetails.title}
-              onChange={(event) => updateDetails({ title: event.target.value })}
+            {note ? "Edit Note" : "Create Note"}
+          </BootstrapDialogTitle>
+
+          {notesStatus === "loading" && <LinearProgress />}
+          <DialogContent
+            dividers
+            sx={{
+              padding: 0,
+              maxHeight: "60vh",
+              borderColor: fontColor(noteDetails.color),
+            }}
+          >
+            <Container
+              maxWidth="xl"
               sx={{
-                marginTop: 4,
-              }}
-            />
-            <Divider
-              sx={{
-                marginTop: 2,
-                marginBottom: 2,
-                "&.MuiDivider-root": {
-                  borderTop: `1px solid ${fontColor(noteDetails.color)}`,
+                flexBasis: "auto",
+                height: "100%",
+                overflow: "auto",
+                background: COLORS[noteDetails.color],
+                "& .MuiInputBase-root, & .MuiInputLabel-root": {
+                  color: fontColor(noteDetails.color),
+                },
+                "& .MuiOutlinedInput-root": {
+                  "& fieldset, &:hover fieldset, &.Mui-focused fieldset": {
+                    borderColor: fontColor(noteDetails.color),
+                  },
                 },
               }}
-            ></Divider>
-            <TextareaAutosize
-              minRows={4}
-              aria-label="notes content"
-              placeholder="Enter your content here"
-              value={noteDetails.content}
-              onChange={(event) =>
-                updateDetails({ content: event.target.value })
-              }
-              style={{
-                width: "100%",
-                height: "calc(100% - 150px)",
-                background: COLORS[noteDetails.color],
+            >
+              <TextField
+                fullWidth
+                label="Title"
+                id="title"
+                value={noteDetails.title}
+                onChange={(event) =>
+                  updateDetails({ title: event.target.value })
+                }
+                sx={{
+                  marginTop: 2,
+                }}
+              />
+              <Divider
+                sx={{
+                  marginTop: 2,
+                  marginBottom: 2,
+                  "&.MuiDivider-root": {
+                    borderTop: `1px solid ${fontColor(noteDetails.color)}`,
+                  },
+                }}
+              ></Divider>
+              <TextareaAutosize
+                minRows={4}
+                aria-label="notes content"
+                placeholder="Enter your content here"
+                value={noteDetails.content}
+                onChange={(event) =>
+                  updateDetails({ content: event.target.value })
+                }
+                style={{
+                  width: "100%",
+                  height: "calc(100% - 150px)",
+                  background: COLORS[noteDetails.color],
+                  color: fontColor(noteDetails.color),
+                  border: `1px solid ${fontColor(noteDetails.color)}`,
+                  borderRadius: "4px",
+                  padding: "14px",
+                  fontSize: "1rem",
+                  marginBottom: "16px",
+                }}
+              />
+            </Container>
+          </DialogContent>
+          <DialogActions
+            sx={{
+              background: COLORS[noteDetails.color],
+              color: fontColor(noteDetails.color),
+              "& .MuiButtonBase-root": {
                 color: fontColor(noteDetails.color),
-                border: `1px solid ${fontColor(noteDetails.color)}`,
-                borderRadius: "4px",
-                padding: "14px",
-                fontSize: "1rem",
-              }}
-            />
-          </Container>
+              },
+            }}
+          >
+            <Button
+              autoFocus
+              color="inherit"
+              onClick={() => handleSave(noteDetails, note ? "edit" : "add")}
+            >
+              Save Changes
+            </Button>
+          </DialogActions>
         </Box>
       </Dialog>
     </div>
